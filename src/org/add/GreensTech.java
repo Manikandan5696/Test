f7cbf6785ca82f0ad63a80e238a1e8da5aeae2cd
Manikandan5696 <learningonprocess@gmail.com>
2021-03-26 11:28:46 +0530

    New Project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,13 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOMR() {
+		System.out.println("Greens Technology, OMR");
+	}
+
+	public void greensAdyar() {
+		System.out.println("Greens Technology, Adyar");
+	}
+
+	
+}
